e06d1900f77b3f92cb842acca5ef1274c7ee60d9
Anya Mares <anyavanessamares@gmail.com>
2025-07-31 16:53:56 -0600

    se agrego tabla de listas

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ const btnClear = document.getElementById("btnClear");
 
 const alertValidaciones = document.getElementById("alertValidaciones");
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const tablaListaCompras = document.getElementById("tablaListaCompras");
+const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
+
+
+let cont = 0;
 
 function ValidarCantidad(){
     if (txtNumber.value.length==0 ){
@@ -18,7 +23,7 @@ function ValidarCantidad(){
     if(Number(txtNumber.value)<=0){
         return false;
     }//Mayor que 0
-    
+
         return true;
 }//validar cantidad
 
@@ -26,8 +31,9 @@ function getPrecio(){
     return Math.round(Math.random()* 10000)/100;
 }//getPrecio 
 
-btnAgregar.addEventListener("click",function(event){
+btnAgregar.addEventListener("click", function(event){
  event.preventDefault();
+ let isValid = true; 
  alertValidacionesTexto.innerHTML="";
  alertValidaciones.style.display="none";
  txtName.style.border="";
@@ -40,13 +46,33 @@ if(txtName.value.length<3){
     //mensaje de error 
 alertValidacionesTexto.innerHTML = "<strong>El Nombre del producto no es correcto</strong>";
 alertValidaciones.style.display="block";
+isValid =false;
 }//<3
 
 if(! ValidarCantidad()){
-    txtNumber.style.border= "medium red solid";
+    txtNumber.style.border= "thin red solid";
     alertValidacionesTexto.innerHTML += "<strong>La Cantidad no es correcta</strong>";
     alertValidaciones.style.display= "block";
+    isValid =false;
 }//validar cantidad
+
+if (isValid){
+cont++;
+let precio = getPrecio();
+let row=`<tr>
+<td>${cont}</td>
+<td>${txtName.value}</td>
+<td>${txtNumber.value}</td>
+<td>${precio}</td>
+</tr>`
+;
+
+cuerpoTabla.insertAdjacentHTML("beforeend", row);
+txtName.value ="";
+txtNumber.value ="";
+txtName.focus();
+
+}//isValid
 
  //Number
  //Tenga informacion
